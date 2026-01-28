ce17d9a1cc2e89eafd8113d06664eb45f0aca006
Sebastian Gonzalez <seba14079783@gmail.com>
2026-01-28 16:21:04 -0300

    Se ajusta navegador para el pipeline

--- src/main/java/config/DriverFactory.java
+++ src/main/java/config/DriverFactory.java
@@ -2,7 +2,10 @@ package config;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DriverFactory {
@@ -14,21 +17,53 @@ public class DriverFactory {
 
     public static void initDriver(String browser) {
         if (driver == null) {
+
+            boolean isCI = "true".equalsIgnoreCase(System.getenv("CI"));
+
             switch (browser.toLowerCase()) {
+
                 case "chrome":
                     WebDriverManager.chromedriver().setup();
-                    driver = new ChromeDriver();
+                    ChromeOptions chromeOptions = new ChromeOptions();
+
+                    if (isCI) {
+                        chromeOptions.addArguments(
+                            "--headless=new",
+                            "--no-sandbox",
+                            "--disable-dev-shm-usage",
+                            "--disable-gpu",
+                            "--window-size=1920,1080"
+                        );
+                    }
+
+                    driver = new ChromeDriver(chromeOptions);
                     break;
+
                 case "edge":
                     WebDriverManager.edgedriver().setup();
-                    driver = new EdgeDriver();
+                    EdgeOptions edgeOptions = new EdgeOptions();
+
+                    if (isCI) {
+                        edgeOptions.addArguments(
+                            "--headless=new",
+                            "--no-sandbox",
+                            "--disable-dev-shm-usage",
+                            "--disable-gpu",
+                            "--window-size=1920,1080"
+                        );
+                    }
+
+                    driver = new EdgeDriver(edgeOptions);
                     break;
+
                 default:
                     throw new IllegalArgumentException("Navegador no soportado: " + browser);
             }
+
             driver.manage().window().maximize();
         }
     }
+
 
     public static void quitDriver() {
         if (driver != null) {
